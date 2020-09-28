e03faf0644543584f7b4dad3b1b90b729d29e293
CueToDo <alex@Q2Do.com>
2020-09-28 12:48:31 +0100

    gofundme proxy.conf.json css for mobile

--- src/app/take-action/take-action.component.ts
+++ src/app/take-action/take-action.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakeActionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit(): void {
   }
