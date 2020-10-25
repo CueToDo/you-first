16720bf7ab960130df3ca33d13bafd296a762784
CueToDo <alex@Q2Do.com>
2020-10-25 19:28:30 +0000

    PR It's not cricket

--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
 @Component({
   selector: 'app-more',
   templateUrl: './more.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private document
+  ) { }
 
   ngOnInit(): void {
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://platform.twitter.com/widgets.js';
+    s.text = ``;
+    this.renderer2.appendChild(this.document.body, s);
   }
 
 }
